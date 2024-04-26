97556e59defa58890855fbb67246b609534e2d0e
fuong08 <152625972+fuong08@users.noreply.github.com>
2024-04-26 09:12:16 +0700

    icon xinh xinh

--- src/main/java/DictionaryMain/Test.java
+++ src/main/java/DictionaryMain/Test.java
@@ -1,6 +1,6 @@
 package DictionaryMain;
 
-public class Test {
+public class 2Test {
     public static void main(String[] args) {
         DictionaryManagement dict = new DictionaryManagement();
         DictionaryCommandline DictionaryCommandLine = null;
