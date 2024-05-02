8707d9e82e57707c24800eaef5047aa362170125
fuong08 <152625972+fuong08@users.noreply.github.com>
2024-05-02 10:50:26 +0700

    app

--- src/main/java/DictionaryMain/Test.java
+++ src/main/java/DictionaryMain/Test.java
@@ -1,9 +1,9 @@
 package DictionaryMain;
 
-public class 2Test {
+public class Test {
     public static void main(String[] args) {
         DictionaryManagement dict = new DictionaryManagement();
-
+        DictionaryCommandline DictionaryCommandLine = new DictionaryCommandline();
         DictionaryCommandLine.dictionaryBasic(dict);
         DictionaryCommandLine.dictionaryAdvanced(dict);
         DictionaryCommandLine.dictionarySearcher(dict);
